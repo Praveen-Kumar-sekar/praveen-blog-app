3495f81d4ce829bc923bc745d1c95ef4607acfe6
praveenkumatsekar <praveen0077kumar@gmail.com>
2023-06-02 22:46:32 +0530

    section 3 completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'category', component: SingleCategoryComponent},
+  {path:'post', component: SinglePostComponent},
+
+ {path:'term-conditions', component: TermsAndConditionComponent},
+ {path:'contact', component: ContactUsComponent},
+ {path:'about', component:AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
